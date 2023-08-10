e6c1fea379b1919175fe494bccdc50379a5b707a
ehuusko <ehuusko@csc.fi>
2023-08-10 10:34:45 +0300

    Modified scripts and completed collections

--- geocubes_stac.py
+++ geocubes_stac.py
@@ -1,135 +1,198 @@
 import pystac
-from rio_stac.stac import create_stac_item
-from shapely.geometry import GeometryCollection, shape
 import rasterio
 import requests
-from pathlib import Path
 import datetime
+import pandas
+import re
 from bs4 import BeautifulSoup
+from rio_stac.stac import create_stac_item
+from shapely.geometry import GeometryCollection, shape
+from pathlib import Path
 
-vrts = [
-    "https://vm0160.kaj.pouta.csc.fi/mml/korkeusmalli/km2/2022/km2_2022.vrt",
-    "https://vm0160.kaj.pouta.csc.fi/mml/korkeusmalli/km2/2020/km2_2020.vrt",
-    "https://vm0160.kaj.pouta.csc.fi/mml/korkeusmalli/km2/2019/km2_2019.vrt",
-    "https://vm0160.kaj.pouta.csc.fi/mml/korkeusmalli/km2/2018/km2_2018.vrt"
-]
+def create_collection(collection_info, dataset_info):
 
-year_collections = [
-    "https://vm0160.kaj.pouta.csc.fi/mml/korkeusmalli/km2/2022/",
-    "https://vm0160.kaj.pouta.csc.fi/mml/korkeusmalli/km2/2020/",
-    "https://vm0160.kaj.pouta.csc.fi/mml/korkeusmalli/km2/2019/",
-    "https://vm0160.kaj.pouta.csc.fi/mml/korkeusmalli/km2/2018/"
-]
+    col_name = re.sub('\W+','_', collection_info['Name'].lower())
+    col_id = f"{col_name}_at_geocubes"
 
-#TIFs through VRTs
-# for vrt in vrts:
-#     with rasterio.open(vrt) as r:
-#         files = r.files
-#         tifs = {}
-#         for i,file in enumerate(files):
-#             if file.endswith("tif"):
-#                 tifs[file.split("/vsicurl/")[-1].split("/")[-1]] = file.split("/vsicurl/")[-1]
-
-catalog = pystac.Catalog("GeoCubes", "Testing catalog", catalog_type=pystac.CatalogType.RELATIVE_PUBLISHED)
-collection = pystac.Collection(
-    id = "km2_at_geocubes",
-    title = "MML Korkeusmalli Test",
-    description = "Testing collection for MML Korkeusmalli GeoCubes",
-    license  = "CC-BY-4.0",
-    extent = pystac.Extent(
-        spatial = pystac.SpatialExtent([[0,0,0,0]]), #Placeholder extent, updated from items later
-        temporal = pystac.TemporalExtent([(
-            datetime.datetime.strptime("2018-01-01", "%Y-%m-%d"), 
-            datetime.datetime.strptime("2022-12-31", "%Y-%m-%d")
-        )])
-    ),
-    providers = [
-        pystac.Provider(
-            name = "CSC Finland",
-            url = "https://www.csc.fi/",
-            roles = ["host"]
+    collection = pystac.Collection(
+        id = col_id,
+        title = collection_info['Name'],
+        description = collection_info['Description'],
+        license = "CC-BY-4.0",
+        extent = pystac.Extent( #Placeholder extents, updated from items later
+            spatial = pystac.SpatialExtent([[0,0,0,0]]),
+            temporal = pystac.TemporalExtent([(
+                datetime.datetime.strptime(f"2000-01-01", "%Y-%m-%d"),
+                datetime.datetime.strptime(f"2000-12-31", "%Y-%m-%d")
+            )])
         ),
-        pystac.Provider(
-            name = "NLS/FGI",
-            roles = ["processor"]
-        )
-    ],
-    summaries = pystac.Summaries(
-        summaries = {
-            "gsd": [1,2,5,10,20,50,100,200,500,1000]
-        }
-    )
-)
-print(f"Collection made: {collection.id}")
-catalog.add_child(collection)
-
-#TIFs through BeautifulSoup
-for year in year_collections:
-
-    page = requests.get(year)
-    data = page.text
-    soup = BeautifulSoup(data, features="html.parser")
-
-    links = [link for link in soup.find_all("a")]
-
-    assets = {}
-    item_links = [link.get("href") for link in links if link.get("href").endswith("tif")]
-    item_sets = [item.split(".")[0] for item in item_links]
-    
-    grouped_dict = {}
-    for item in item_sets:
-        prefix = "_".join(item.split("_")[:4])
-        if prefix not in grouped_dict:
-            grouped_dict[prefix] = []
-        grouped_dict[prefix].append(item)
-
-    for key in grouped_dict.keys():
-        assets = {
-            "COG": pystac.Asset(
-                href=year+grouped_dict[key][0]+".tif", 
-                media_type="image/tiff; application=geotiff; profile=cloud-optimized", 
-                title="COG",
-                roles=["data"]
+        providers = [
+            pystac.Provider(
+                name = "CSC Finland",
+                url = "https://www.csc.fi/",
+                roles = ["host"]
+            ),
+            pystac.Provider(
+                name = dataset_info['producer'],
+                roles = ["producer"]
             )
-        }
-        for asset in grouped_dict[key][1:]:
-            with rasterio.open(year+asset+".tif") as src:
-                assets[asset.split("_")[-1]] = pystac.Asset(
-                    href=year+asset+".tif",
-                    media_type="image/tiff; application=geotiff", 
-                    title=asset.split("_")[-1],
-                    roles=["data"],
-                    extra_fields={
-                        "gsd": int(src.res[0]),
-                        "proj:shape": src.shape,
-                        "proj:transform": [
-                            src.transform.a,
-                            src.transform.b,
-                            src.transform.c,
-                            src.transform.d,
-                            src.transform.e,
-                            src.transform.f,
-                            src.transform.g,
-                            src.transform.h,
-                            src.transform.i
-                        ]
-                    }
-                )
-        item_year = key.split("_")[1]
-        item = create_stac_item(
-            year+key+".tif", 
-            assets=assets, 
-            asset_media_type=pystac.MediaType.TIFF, 
-            with_proj=True, 
-            input_datetime = datetime.datetime.strptime(f"{item_year}-12-31", "%Y-%m-%d")
+        ],
+        assets = {
+            "meta": pystac.Asset(
+                dataset_info["metadata_URL"],
+                title = "Metadata",
+                roles = ["metadata"]
+            )
+        },
+        summaries = pystac.Summaries(
+            summaries = {
+                "gsd": []
+            }
         )
-        item.id = item.id.split(".")[0]
-        item.extra_fields["gsd"] = 1
-        assert item.validate()
-        collection.add_item(item)
-        print(f"* Item made: {item.id}")
+    )
+
+    print(f"Collection made: {collection.id}")
+    
+    return collection
+
+def get_datasets():
+
+    data = requests.get("https://vm0160.kaj.pouta.csc.fi/geocubes/info/getDatasets")
+    const_url = "https://vm0160.kaj.pouta.csc.fi"
+    raw_datasets = data.text.split(";")
+    split_datasets = [x.split(",") for x in raw_datasets]
+
+    dataset_dict = {}
+    for split in split_datasets:
+        dataset_dict[split[0]] = dict(zip(["name", "layername", "years", "folder", "file_prefix", "max_resolution", "bit_depth", "producer", "metadata_URL"], split))
+
+    for d in dataset_dict:
+        year_split = dataset_dict[d]['years'].split(".")
+        dataset_dict[d]['paths'] = []
+        if len(year_split) == 1:
+            dataset_dict[d]['paths'].append(f"{const_url}{dataset_dict[d]['folder']}{year_split[0]}/")
+        else:
+            for year in year_split:
+                dataset_dict[d]['paths'].append(f"{const_url}{dataset_dict[d]['folder']}{year}/")
+    
+    return dataset_dict
+
+datasets = get_datasets()
+try:
+    catalog = pystac.Catalog("GeoCubes", "Testing catalog", catalog_type=pystac.CatalogType.RELATIVE_PUBLISHED)
+except:
+    catalog = pystac.Catalog.from_file("Geocubes/catalog.json")
+
+collection_csv = pandas.read_csv('karttatasot.csv', index_col='Nimi').to_dict('index')
+
+for col in collection_csv:
+
+    collection_info = collection_csv[col]
+    dataset_info = datasets[col]
+    print(collection_info)
+    print(dataset_info)
+
+    collection = create_collection(collection_info, dataset_info)
+    catalog.add_child(collection)
+    
+    for year_path in dataset_info['paths']:
+
+        #TIFs through BeautifulSoup
+        page = requests.get(year_path)
+        data = page.text
+        soup = BeautifulSoup(data, features="html.parser")
+
+        links = [link for link in soup.find_all("a")]
+
+        assets = {}
+        item_links = [link.get("href") for link in links if link.get("href").endswith("tif")]
+        item_sets = [item.split(".")[0] for item in item_links]
+        
+        grouped_dict = {}
+        for item in item_sets:
+            prefix = "_".join(item.split("_")[:4])
+            if prefix not in grouped_dict:
+                grouped_dict[prefix] = []
+            grouped_dict[prefix].append(item)
+
+        for key in grouped_dict.keys():
+
+            item_starttime = datetime.datetime.strptime(f"{year_path.split('/')[-2]}-01-01", "%Y-%m-%d")
+            item_endtime = datetime.datetime.strptime(f"{year_path.split('/')[-2]}-12-31", "%Y-%m-%d")
+
+            with rasterio.open(year_path+grouped_dict[key][0]+".tif") as src:
+                assets = {
+                    "COG": pystac.Asset(
+                        href=year_path+grouped_dict[key][0]+".tif", 
+                        media_type="image/tiff; application=geotiff; profile=cloud-optimized", 
+                        title="COG",
+                        roles=["data"],
+                        extra_fields={
+                            "gsd": int(src.res[0]),
+                            "proj:shape": src.shape,
+                            "proj:transform": [
+                                src.transform.a,
+                                src.transform.b,
+                                src.transform.c,
+                                src.transform.d,
+                                src.transform.e,
+                                src.transform.f,
+                                src.transform.g,
+                                src.transform.h,
+                                src.transform.i
+                            ]
+                        }
+                    )
+                }
+            min_gsd = assets["COG"].extra_fields["gsd"]
+            for asset in grouped_dict[key][1:]:
+                with rasterio.open(year_path+asset+".tif") as src:
+                    asset_id = asset.split("_")[-1]
+                    assets[asset_id] = pystac.Asset(
+                        href=year_path+asset+".tif",
+                        media_type="image/tiff; application=geotiff", 
+                        title=asset.split('_')[-1],
+                        roles=["data"],
+                        extra_fields={
+                            "gsd": int(src.res[0]),
+                            "proj:shape": src.shape,
+                            "proj:transform": [
+                                src.transform.a,
+                                src.transform.b,
+                                src.transform.c,
+                                src.transform.d,
+                                src.transform.e,
+                                src.transform.f,
+                                src.transform.g,
+                                src.transform.h,
+                                src.transform.i
+                            ]
+                        }
+                    )
+                if assets[asset_id].extra_fields["gsd"] not in collection.summaries.lists["gsd"]:
+                    collection.summaries.lists["gsd"].append(assets[asset_id].extra_fields["gsd"])
+                min_gsd = min(min_gsd, assets[asset_id].extra_fields["gsd"])
+
+            item_year = key.split("_")[1]
+            item = create_stac_item(
+                year_path+key+".tif", 
+                assets=assets, 
+                asset_media_type=pystac.MediaType.TIFF, 
+                with_proj=True, 
+            )
+            item_info = "_".join(item.id.split(".")[0].split("_")[1:])
+            item.id = f"{collection_info['Name'].lower().replace(' ', '_').replace(',', '')}_{item_info}"
+            item.common_metadata.start_datetime = item_starttime
+            item.common_metadata.end_datetime = item_endtime
+            item.extra_fields["gsd"] = min_gsd
+            collection.add_item(item)
+            print(f"* Item made: {item.id}")
 
     bounds = [GeometryCollection([shape(s.geometry) for s in collection.get_all_items()]).bounds]
+    start_times = [st.common_metadata.start_datetime for st in collection.get_all_items()]
+    end_times = [et.common_metadata.end_datetime for et in collection.get_all_items()]
+    temporal = [[min(start_times), max(end_times)]]
     collection.extent.spatial = pystac.SpatialExtent(bounds)
+    collection.extent.temporal = pystac.TemporalExtent(temporal)
 
-catalog.normalize_and_save("mml-korkeusmalli")
+catalog.normalize_and_save("GeoCubes")
---
+++ geocubes_to_geoserver.py
@@ -0,0 +1,182 @@
+import json
+import getpass
+import argparse
+import requests
+import pystac_client
+from pathlib import Path
+from requests.auth import HTTPBasicAuth
+from urllib.parse import urljoin
+
+def change_to_https(request: requests.Request) -> requests.Request: 
+    request.url = request.url.replace("http:", "https:")
+    return request
+
+def json_convert(jsonfile):
+
+    """
+        jsonfile: json file in dict format
+        
+        A function to map the Sentinel-2 STAC jsonfiles into the GeoServer database layout.
+        There are different json layouts for Collections and Items. The function checks if the jsonfile is of type "Collection",
+        or of type "Feature" (=Item). A number of properties are hardcoded into Sentinel-2 metadata as these are not collected in the STAC jsonfiles.
+    """
+
+    with open(jsonfile) as f:
+        content = json.load(f)
+    
+    if content["type"] == "Collection":
+
+        new_json = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Polygon",
+                "coordinates": [
+                    [
+                        [
+                            content["extent"]["spatial"]["bbox"][0][2],
+                            content["extent"]["spatial"]["bbox"][0][1]
+                        ],
+                        [
+                            content["extent"]["spatial"]["bbox"][0][2],
+                            content["extent"]["spatial"]["bbox"][0][3]
+                        ],
+                        [
+                            content["extent"]["spatial"]["bbox"][0][0],
+                            content["extent"]["spatial"]["bbox"][0][3]
+                        ],
+                        [
+                            content["extent"]["spatial"]["bbox"][0][0],
+                            content["extent"]["spatial"]["bbox"][0][1]
+                        ],
+                        [
+                            content["extent"]["spatial"]["bbox"][0][2],
+                            content["extent"]["spatial"]["bbox"][0][1]
+                        ]
+
+                    ]
+                ]
+            },
+            "properties": {
+                "name": content["id"],
+                "title": content["title"],
+                "eo:identifier": content["id"],
+                "description": content["description"],
+                "timeStart": content["extent"]["temporal"]["interval"][0][0],
+                "timeEnd": content["extent"]["temporal"]["interval"][0][1],
+                "primary": True,
+                "license": content["license"],
+                "providers": content["providers"], # Providers added
+                "derivedFrom": None,
+                "licenseLink": None,
+                "summaries": content["summaries"],
+                "queryables": [
+                    "eo:identifier"
+                ]
+            }
+        }
+
+        if "derived_from" in content:
+            new_json["properties"]["derivedFrom"] = {
+                    "href": content["derived_from"],
+                    "rel": "derived_from",
+                    "type": "application/json"
+            }
+
+        if "assets" in content:
+            new_json["properties"]["assets"] = content["assets"]
+
+        for link in content["links"]:
+            if link["rel"] == "license":
+                new_json["properties"]["licenseLink"] = {
+                    "href": link["href"],
+                    "rel": "license",
+                    "type": "application/json"
+                } # New License URL link
+
+    if content["type"] == "Feature":
+
+        new_json = {
+            "type": "Feature",
+            "geometry": content["geometry"],
+            "properties": {
+                "eop:identifier": content["id"],
+                "eop:parentIdentifier": content["collection"],
+                "timeStart": content["properties"]["start_datetime"],
+                "timeEnd": content["properties"]["end_datetime"],
+                "eop:resolution": content["gsd"],
+                # "opt:cloudCover": int(content["properties"]["eo:cloud_cover"]),
+                "crs": content["properties"]["proj:epsg"],
+                "projTransform": content["properties"]["proj:transform"],
+                # "thumbnailURL": content["links"]["thumbnail"]["href"],
+                "assets": content["assets"]
+            }
+        }
+
+    return json.loads(json.dumps(new_json))
+
+if __name__ == "__main__":
+
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--host", type=str, help="Hostname of the selected STAC API", required=True)
+    
+    # args = parser.parse_args()
+
+    pwd = getpass.getpass()
+
+    collection_name = "wind_velocity_at_geocubes"
+
+    workingdir = Path(__file__).parent
+    sentinel = workingdir / "GeoCubes" / collection_name
+
+    app_host = "http://86.50.229.158:8080/geoserver/rest/oseo/"
+    catalog = pystac_client.Client.open("http://86.50.229.158:8080/geoserver/ogc/stac/")#, request_modifier=change_to_https)
+
+    # Convert the STAC collection json into json that GeoServer can handle
+    converted = json_convert(sentinel / "collection.json")
+
+    #Additional code for changing collection data if the collection already exists
+    collections = catalog.get_collections()
+    col_ids = [col.id for col in collections]
+    if collection_name in col_ids:
+        r = requests.put(urljoin(app_host + "collections/", collection_name), json=converted, auth=HTTPBasicAuth("admin", pwd))
+        r.raise_for_status()
+        print(f"Updated {collection_name}")
+    else:
+        r = requests.post(urljoin(app_host, "collections/"), json=converted, auth=HTTPBasicAuth("admin", pwd))
+        r.raise_for_status()
+        print(f"Added new collection")
+
+    # Get the posted items from the specific collection
+    posted = catalog.search(collections=[collection_name]).item_collection()
+    posted_ids = [x.id for x in posted]
+    print(f"POSTed: {len(posted_ids)}")
+
+    with open(sentinel / "collection.json") as f:
+        rootcollection = json.load(f)
+
+    items = [x['href'] for x in rootcollection["links"] if x["rel"] == "item"]
+
+    print("POSTing items:")
+    for i, item in enumerate(items):
+        with open(sentinel / item) as f:
+            payload = json.load(f)
+        # Convert the STAC item json into json that GeoServer can handle
+        converted = json_convert(sentinel / item)
+        request_point = f"collections/{rootcollection['id']}/products"
+        if payload["id"] in posted_ids:
+            request_point = f"collections/{rootcollection['id']}/products/{payload['id']}"
+            r = requests.put(urljoin(app_host, request_point), json=converted, auth=HTTPBasicAuth("admin", pwd))
+            r.raise_for_status()
+        else:
+            r = requests.post(urljoin(app_host, request_point), json=converted, auth=HTTPBasicAuth("admin", pwd))
+            r.raise_for_status()
+        if len(items) >= 5: # Just to keep track that the script is still running
+            if i == int(len(items) / 5):
+                print("~20% of items added")
+            elif i == int(len(items) / 5) * 2:
+                print("~40% of items added")
+            elif i == int(len(items) / 5) * 3:
+                print("~60% of items added")
+            elif i == int(len(items) / 5) * 4:
+                print("~80% of items added")
+    print("")
